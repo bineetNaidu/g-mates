963007b587e10d4b54f5fa9186ee2439f97f6a8b
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-02-02 20:49:04 +0530

    chore: :sparkles: add data-source pre initialization

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { AppDataSource } from '@/lib/data-source';
+import { CategoryResolvers } from '@/lib/graphql/category/category.resolver';
 import { HelloResolver } from '@/lib/graphql/greet/greetings.resolver';
-import { createYoga, YogaServerInstance } from 'graphql-yoga';
+import { createYoga } from 'graphql-yoga';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import 'reflect-metadata';
 import { buildSchema } from 'type-graphql';
@@ -15,7 +16,7 @@ export const config = {
 
 const schema = buildSchema({
   validate: false,
-  resolvers: [HelloResolver],
+  resolvers: [HelloResolver, CategoryResolvers],
 });
 
 type Meta = {
@@ -25,7 +26,11 @@ type Meta = {
 
 type GqlContext = Meta & {};
 
-const yogaServer = createYoga<Meta, GqlContext>({
+if (!AppDataSource.isInitialized) {
+  AppDataSource.initialize();
+}
+
+export default createYoga<Meta, GqlContext>({
   schema,
   // Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
   landingPage: true,
@@ -34,13 +39,3 @@ const yogaServer = createYoga<Meta, GqlContext>({
     return { req, res };
   },
 });
-
-const prepareDb = async (server: YogaServerInstance<Meta, Meta>) => {
-  if (!AppDataSource.isInitialized) {
-    await AppDataSource.initialize();
-  }
-
-  return server;
-};
-
-export default prepareDb(yogaServer);
