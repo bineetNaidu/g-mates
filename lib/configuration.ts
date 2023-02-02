25b2728dd10fac57e2d18f6aef1f581cebb27fc3
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-02-02 20:38:48 +0530

    chore: :bug: update config

--- lib/configuration.ts
+++ lib/configuration.ts
@@ -3,7 +3,11 @@ import { z } from 'zod';
 const zodConfiguration = z.object({
   environment: z.enum(['development', 'test', 'production']),
   database: z.object({
-    uri: z.string(),
+    host: z.string(),
+    port: z.number(),
+    username: z.string(),
+    password: z.string(),
+    name: z.string(),
   }),
   nextAuth: z.object({
     secret: z.string(),
@@ -19,7 +23,11 @@ export type Configuration = z.infer<typeof zodConfiguration>;
 export const configuration = zodConfiguration.parse({
   environment: process.env.NODE_ENV || 'development',
   database: {
-    uri: process.env.DATABASE_URI!,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
   },
   nextAuth: {
     secret: process.env.AUTH_SECRET!,
